5bb219030a9b19da274ff2938bebad6f0ffa2626
Sarasi <sarasividumini13@gmail.com>
2025-09-16 14:24:11 +0530

    18th Commit - Created Instructor.fxml using images. and Updated MainPage.fxml changes.

---
+++ src/main/java/lk/ijse/orm_final_coursework/controller/ManageInstructorController.java
@@ -0,0 +1,48 @@
+package lk.ijse.orm_final_coursework.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class ManageInstructorController {
+
+    public TableView tblInstructors;
+    public TableColumn colInstructorId;
+    public TableColumn colFirstName;
+    public TableColumn colLastName;
+    public TableColumn colEmail;
+    public TableColumn colPhone;
+    public TableColumn colAvailabilityShedule;
+    public TextField txtSearch;
+    public Button btnReset;
+    public Button btnDelete;
+    public Button btnUpdate;
+    public Button btnSave;
+    public TextField txtAvailabilityShedule;
+    public TextField txtSpecialization;
+    public TextField txtPhone;
+    public TextField txtEmail;
+    public TextField txtLastName;
+    public TextField txtFirstName;
+    public AnchorPane ancInstructorPage;
+    public Label lblInstructorId;
+
+    public void goToDashboard(MouseEvent mouseEvent) {
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnResetOnAction(ActionEvent actionEvent) {
+    }
+
+    public void OnClickedTable(MouseEvent mouseEvent) {
+    }
+}
